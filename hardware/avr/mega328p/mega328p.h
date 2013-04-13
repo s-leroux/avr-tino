588c12bfd92badc0fcf7179653229840c3bacbea
Sylvain Leroux <sylvain@chicoree.fr>
2013-04-13 11:32:52 +0200

    Added support for ATmega328P

---
+++ hardware/avr/mega328p/mega328p.h
@@ -0,0 +1,29 @@
+/*
+  Copyright (c) 2012-2013 Sylvain Leroux <sylvain@chicoree.fr>
+  
+  This file is part of avr-tino -- http://github.com/s-leroux/avr-tino
+  
+  avr-tino is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  avr-tino is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with avr-tino.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#if !defined AVR_TINO_MEGA328P_H
+#define AVR_TINO_MEGA328P_H
+
+/*
+ Support for ATmega328P MCU
+*/
+
+#include "avr-tino/../../hardware/avr/mega328/mega328.h"
+
+#endif
+
